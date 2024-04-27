a7a1bf1128cfcdbf9560ccae0888c63007b9d7b1
Mukhammadibrokhim <tursunboyev06061999@gmail.com>
2024-04-27 21:15:26 +0500

    First vue js project (Hello world)

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="container">
+        <h1>Counter: {{ counter }}</h1>
+        <div class="d-flex">
+         <button @click="onIncreament" type="button" class="btn btn-outline-success">Addition</button>
+         <button @click="onDecrement" type="button" class="btn btn-outline-danger">Decrement</button>
+        </div>
+    </div>
+</template>
+
+<script>
+ export default {
+    data() {
+        return {
+            counter: 0
+        }
+    },
+    methods: {
+        onIncreament(){
+            this.counter +=1
+        },
+        onDecrement(){
+            this.counter -=1
+        }
+    },  
+ }
+</script>
+
+<style>
+
+</style>
